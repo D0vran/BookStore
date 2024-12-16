96af3339d377d6f6d7c50f28bf067ea9b681d889
D0wran <dowran20d3@gmail.com>
2024-12-16 17:05:41 +0500

    changes

--- src/Acme.BookStore.Domain/AuthorManager.cs
+++ src/Acme.BookStore.Domain/AuthorManager.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Acme.BookStore.Authors;
+using Volo.Abp;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Domain.Services;
 
 namespace Acme.BookStore
 {
-    internal class AuthorManager
+    public class AuthorManager : DomainService
     {
+        private readonly IAuthorRepository<Author, Guid> _authorRepository;
+
+        public AuthorManager(IAuthorRepository<Author, Guid> authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public async Task<Author> CreateAsync   
+                (Guid id,
+                string name,
+                DateTime dateOfBirth,
+                string? shortBio)
+                    
+        {
+            Check.NotNullOrWhiteSpace(name, nameof(name));
+
+            var existingAuthor = await _authorRepository.FindByNameAsync(name);
+
+            if(existingAuthor is not null)
+            {
+                throw new AuthorAlreadyExistsException(name);
+            }
+
+            return new Author
+                    (
+                        GuidGenerator.Create(),
+                        name,
+                        dateOfBirth,
+                        shortBio
+                    );
+
+        }
+
+        public async Task ChangeNameAsync(Author author, string newName)
+        {
+            Check.NotNullOrWhiteSpace (newName, nameof(newName));
+            Check.NotNull(author, nameof(author));
+            
+            var existingAuthor = _authorRepository.FindByNameAsync(newName);
+
+            if (existingAuthor is not null or existingAuthor.id != author.Id)
+            {
+                throw new AuthorAlreadyExistsException(newName);
+            }
+
+            author.ChangeName(newName);
+        }
     }
 }
